96e8af39d0c2ae5977cffd33c2b492c12cced49b
bitowl <git@bitowl.de>
2019-08-31 19:17:57 +0200

    Make timer and logo start inivisible on reload

--- graphics/elements/owl-bar/owl-bar.js
+++ graphics/elements/owl-bar/owl-bar.js
@@ -1,108 +1,109 @@
 (function () {
-    'use strict';
-    
-    const barReplicant = nodecg.Replicant('bar', {defaultValue: {
-        timerVisible: false,
-        logoVisible: false
-    }, persistent: true});
+	'use strict';
 
-    const barContentsRepl = nodecg.Replicant('bar-contents');
+	const barReplicant = nodecg.Replicant('bar', {
+		defaultValue: {
+			timerVisible: false,
+			logoVisible: false
+		}, persistent: true
+	});
 
-    class OwlBar extends Polymer.Element {
-        static get is() {
-            return 'owl-bar';
-        }
+	const barContentsRepl = nodecg.Replicant('bar-contents');
 
-        ready() {
-            super.ready();
-            this.isRunning = false;
-            this.changeRequired = false;
+	class OwlBar extends Polymer.Element {
+		static get is() {
+			return 'owl-bar';
+		}
 
-            this.logoVisible = false;
-            this.timerVisible = false;
+		ready() {
+			super.ready();
+			this.isRunning = false;
+			this.changeRequired = false;
 
-            barReplicant.on('change', newVal => {
+			this.logoVisible = false;
+			this.timerVisible = false;
 
-                // this.$.timer.style.display = newVal.timerVisible ? 'block' : 'none';
-                // console.log('display: ' + this.$.eventLogo.style.display);
-                if (newVal.timerVisible && !this.timerVisible) {
-                    this.$.timer.show();
-                    this.timerVisible = true;
-                } else if (!newVal.timerVisible && this.timerVisible) {
-                    this.$.timer.hide();
-                    this.timerVisible = false;
-                }
+			barReplicant.on('change', newVal => {
+				console.log(newVal);
+				// This.$.timer.style.display = newVal.timerVisible ? 'block' : 'none';
+				// console.log('display: ' + this.$.eventLogo.style.display);
+				if (newVal.timerVisible && !this.timerVisible) {
+					this.$.timer.show();
+					this.timerVisible = true;
+				} else if (!newVal.timerVisible && this.timerVisible) {
+					this.$.timer.hide();
+					this.timerVisible = false;
+				}
 
+				if (newVal.logoVisible && !this.logoVisible) {
+					this.$.eventLogo.show();
+					this.logoVisible = true;
+				} else if (!newVal.logoVisible && this.logoVisible) {
+					this.$.eventLogo.hide();
+					this.logoVisible = false;
+				}
 
-                if (newVal.logoVisible && !this.logoVisible) {
-                    this.$.eventLogo.show();
-                    this.logoVisible = true;
-                } else  if (!newVal.logoVisible && this.logoVisible) {
-                    this.$.eventLogo.hide();
-                    this.logoVisible = false;
-                }
-                // this.$.eventLogo.style.display = newVal.logoVisible ? 'block' : 'none'
-                this.eventLogo = newVal.eventLogo;
-            });
+				// This.$.eventLogo.style.display = newVal.logoVisible ? 'block' : 'none'
+				this.eventLogo = newVal.eventLogo;
+			});
 
-            barContentsRepl.on('change', newVal => {
-                if (!this.isRunning) {
-                    this.changeBarContents();
-                    this.run();
-                    return;
-                }
-                this.changeRequired = true; // change when everything is hidden
-            });
-        }
+			barContentsRepl.on('change', newVal => {
+				if (!this.isRunning) {
+					this.changeBarContents();
+					this.run();
+					return;
+				}
 
-        changeBarContents() {
-            var placeholder = this.$.innerBar;
-            while (placeholder.firstChild) { // empty placeholder
-                placeholder.removeChild(placeholder.firstChild);
-            }
+				this.changeRequired = true; // Change when everything is hidden
+			});
+		}
 
-            this.contents = []; 
+		changeBarContents() {
+			const placeholder = this.$.innerBar;
+			while (placeholder.firstChild) { // Empty placeholder
+				placeholder.removeChild(placeholder.firstChild);
+			}
 
-            barContentsRepl.value.forEach(content => {
-                var element = document.createElement('owl-bar-' + content.type);
-                element.options = content.options;
-                placeholder.appendChild(element);
-                this.contents.push(element);
-            });
+			this.contents = [];
 
-        }
+			barContentsRepl.value.forEach(content => {
+				const element = document.createElement('owl-bar-' + content.type);
+				element.options = content.options;
+				placeholder.appendChild(element);
+				this.contents.push(element);
+			});
+		}
 
-        run() {
-            this.isRunning = true;
-            this.currentContent = -1;
-            this.showNextContent();
-        }
+		run() {
+			this.isRunning = true;
+			this.currentContent = -1;
+			this.showNextContent();
+		}
 
-        showNextContent() {
-            this.currentContent++;
+		showNextContent() {
+			this.currentContent++;
 
-            if (this.changeRequired) {
-                this.changeBarContents();
-            }
+			if (this.changeRequired) {
+				this.changeBarContents();
+			}
 
-            if (this.currentContent >= this.contents.length) { 
-                if (this.currentContent == 0) { // There are no bar elements to be displayed
-                    this.isRunning = false;
-                    return;
-                }
-                this.run();// restart
-                return;
-            }
+			if (this.currentContent >= this.contents.length) {
+				if (this.currentContent == 0) { // There are no bar elements to be displayed
+					this.isRunning = false;
+					return;
+				}
 
-            const content = this.contents[this.currentContent];
-            content.enter().to({}, content.options.duration/1000, {}).call(() => {
-                content.exit().call(() => {
-                    this.showNextContent();
-                });
-            });
-        }
-        
+				this.run();// Restart
+				return;
+			}
 
-    }
-    customElements.define(OwlBar.is, OwlBar);
+			const content = this.contents[this.currentContent];
+			content.enter().to({}, content.options.duration / 1000, {}).call(() => {
+				content.exit().call(() => {
+					this.showNextContent();
+				});
+			});
+		}
+	}
+	customElements.define(OwlBar.is, OwlBar);
 })();
